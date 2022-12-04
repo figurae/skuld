05d38204b26449a56c4f53aab576da1d0981a2b7
figurae <aleksander.gielnicki@gmail.com>
2022-12-04 21:16:54 +0100

    Style selector items and switch to NavLinks

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,23 +1,27 @@
 import { ReactNode, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import 'css/Selector.css';
 import { TagContext } from 'contexts/tag-context';
 
 function Selector() {
 	const { tagStorageState } = useContext(TagContext);
 
+	const selectorItemClass = 'selector-item';
+	const selectorItemActiveClass = 'selector-item selector-item-active';
+
 	const tagListNodes: Array<ReactNode> = [];
 	if (tagStorageState !== null) {
 		for (const item of tagStorageState.tagStorage) {
-			// TODO: unhardcode this
 			tagListNodes.push(
-				<Link
+				<NavLink
 					to={'/' + item.tagId}
 					key={item.tagId}
-					style={{ display: 'block' }}
+					className={({ isActive }) =>
+						isActive ? selectorItemActiveClass : selectorItemClass
+					}
 				>
 					{item.tagName}
-				</Link>
+				</NavLink>
 			);
 		}
 	}
@@ -26,9 +30,14 @@ function Selector() {
 		<>
 			<aside className='selector'>
 				<h1 className='text-center'>todo list selector</h1>
-				<Link to='/all' style={{ display: 'block' }}>
+				<NavLink
+					to='/all'
+					className={({ isActive }) =>
+						isActive ? selectorItemActiveClass : selectorItemClass
+					}
+				>
 					all items
-				</Link>
+				</NavLink>
 				{tagListNodes}
 			</aside>
 		</>
