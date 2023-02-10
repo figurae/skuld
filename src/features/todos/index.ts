764ac4f2fd0e59c9333a630106706fbb0afebfae
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2023-02-10 22:16:13 +0100

    Move List and SASS module to src/features/list/

--- src/features/todos/index.ts
+++ src/features/todos/index.ts
@@ -2,6 +2,5 @@ export * from './item';
 export * from './list';
 export * from './tag-menu';
 export { default as Selector } from './Selector';
-export { default as List } from './List';
 export { default as Details } from './Details';
 export { default as Navigator } from './Navigator';
